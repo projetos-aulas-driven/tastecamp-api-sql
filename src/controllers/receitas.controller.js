0e4189712bd8dcb0277de21c7510ea7046c20a61
leticia-chijo <leticia.chijo@gmail.com>
2024-05-08 17:49:45 -0300

    Aula - Conectando o Node ao Postgres

--- src/controllers/receitas.controller.js
+++ src/controllers/receitas.controller.js
@@ -1,19 +1,57 @@
+import { db } from "../database/db.connection.js"
+
 export async function getReceitas(req, res) {
-    res.send("getReceitas")
+    try {
+        const receitas = await db.query(`SELECT * FROM receitas;`)
+        res.send(receitas.rows)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
 export async function getReceitaById(req, res) {
-    res.send("getReceitaById")
+    const { id } = req.params
+    try {
+        const receita = await db.query(`SELECT * FROM receitas WHERE id=$1;`, [id])
+        res.send(receita.rows[0])
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
 export async function createReceita(req, res) {
-    res.send("createReceita")
+    const { titulo, ingredientes, preparo } = req.body
+    try {
+        await db.query(`
+            INSERT INTO receitas (titulo, ingredientes, preparo) 
+                VALUES ($1, $2, $3);
+        `, [titulo, ingredientes, preparo])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
 export async function deleteReceita(req, res) {
-    res.send("deleteReceita")
+    const { id } = req.params
+    try {
+        await db.query(`DELETE FROM receitas WHERE id=$1;`, [id])
+        res.sendStatus(204)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
 
 export async function editReceitaById(req, res) {
-    res.send("editReceitaById")
+    const { id } = req.params
+    const { titulo, ingredientes, preparo } = req.body
+    try {
+        await db.query(`
+            UPDATE receitas SET titulo=$1, ingredientes=$2, preparo=$3
+                WHERE id=$4;
+        `, [titulo, ingredientes, preparo, id])
+        res.sendStatus(200)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
